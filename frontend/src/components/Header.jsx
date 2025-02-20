197437e81f6728e242d78a3913dee5c060ce2a55
sindhu-act <sindhu99.act@gmail.com>
2025-02-20 20:39:15 +0530

    layout page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-//profile pic as logo4
-import logo4 from "../assets/logo4.svg";
-//notification pic as logo5
-import logo5 from "../assets/logo5.svg";
+
+import profile from "../assets/profile.svg";
+import notification from "../assets/notification.svg";
+
 
 
 const Header = () => {
@@ -11,9 +11,10 @@ const Header = () => {
       <h3 className="text-lg font-bold">Dashboard</h3>
       <div className="text-gray-700 ml-150 rounded-lg bg-yellow-100 p-2 w-37">
         Nanny's Shop
+        
       </div>
-      <img src={logo5} alt="Notification" className="h-6 w-6 mr-1" />
-      <img src={logo4} alt="Profile" className="h-6 w-6 mr-1" />
+      <img src={notification} alt="Notification" className="h-6 w-6 mr-1" />
+      <img src={profile} alt="Profile" className="h-6 w-6 mr-1" />
        
     </div>
   );
