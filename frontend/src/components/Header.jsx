f796a1eef993534851f616f78856a9d333c0e34d
sindhu-act <sindhu99.act@gmail.com>
2025-02-21 11:28:37 +0530

    layout page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-
-import profile from "../assets/profile.svg";
+import React, { useState } from "react";
+import { HiChevronDown } from "react-icons/hi";
 import notification from "../assets/notification.svg";
-
-
+import profile from "../assets/profile.svg";
 
 const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="bg-white w-full border-t border-b border-gray-100 p-4 flex justify-between items-center">
       <h3 className="text-lg font-bold">Dashboard</h3>
-      <div className="text-gray-700 ml-150 rounded-lg bg-yellow-100 p-2 w-37">
-        Nanny's Shop
+      
+        <div className="flex items-center text-gray-700 ml-150 rounded-lg bg-yellow-100 p-2 w-37 cursor-pointer">
+          Nanny's Shop
+          <HiChevronDown className="h-5 w-5 ml-2" />
+         
         
       </div>
       <img src={notification} alt="Notification" className="h-6 w-6 mr-1" />
       <img src={profile} alt="Profile" className="h-6 w-6 mr-1" />
-       
     </div>
   );
 };
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { SlArrowRight } from "react-icons/sl";
 import metrixlogo from "../assets/metrixlogo.svg"; 
 import orderlogo from "../assets/orderlogo.svg";
 import customerlogo from "../assets/customerslogo.svg";
 import inventorylogo from "../assets/inventorylogo.svg";
 import conversationslogo from "../assets/conversationslogo.svg";
 import settingslogo from "../assets/settingslogo.svg";
-
 import contactlogo from "../assets/contactlogo.svg";
-
 import giftlogo from "../assets/giftlogo.svg";
+import { IoLogOut } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
@@ -57,23 +56,27 @@ const Sidebar = () => {
             </div>
           </li>
           <li className="mt-4">
-            <div className="flex items-center p-3 hover:bg-gray-200 rounded-lg text-black-500" style={{ backgroundColor: '#5E63661A' }}>
-              <img src={contactlogo} alt="Logo" className="h-6 w-6 mr-2" />
+            <div className="flex items-center w-60 p-3 hover:bg-gray-200 rounded-lg text-black-500" style={{ backgroundColor: '#5E63661A' }}>
+              <img src={contactlogo} alt="Logo" className="h-6 w-4 mr-2" />
               Contact support
             </div>
           </li>
           <li>
-            <div className="flex items-center mt-4 p-4 bg-yellow-100 rounded-lg cursor-pointer">
+            <div className="flex items-center mt-4 p-4 w-60 bg-yellow-100 rounded-lg cursor-pointer">
               <img src={giftlogo} alt="Logo" className="h-6 w-6 mr-3" />
-              <div>
-                <p className="text-black font-medium w-100">Free Gift Awaits You!</p>
-                <p className="text-sm mt-3 pr-50">Upgrade your account</p>
+              <div className="flex items-center justify-between w-full">
+                <div className="whitespace-nowrap">
+                  <p className="text-black text-md">Free Gift Awaits You!</p>
+                  <p className="text-sm">Upgrade your account</p>
+                </div>
+                <SlArrowRight className="h-4 w-3 ml-3 mt-7 " />
               </div>
             </div>
           </li>
         </ul>
-        <div className="mt-4">
-          <div className="w-full p-3 text-red-500 rounded-lg hover:bg-red-100">
+        <div className="mt-5">
+          <div className="p-4 flex items-center text-red-500 rounded-lg hover:bg-red-100 cursor-pointer">
+            <IoLogOut className="h-5 w-5 mr-2" />
             Logout
           </div>
         </div>
